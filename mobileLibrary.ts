218c877bb329f4ad774f297400f2429cb4583e92
NataliSb9 <nsainzbarroso@gmail.com>
2021-02-16 22:33:26 +0100

    MobileLibrary primera parte

---
+++ mobileLibrary.ts
@@ -0,0 +1,54 @@
+import { Mobile } from "./mobile"
+export class MobileLibrary {
+    private name: string;
+    private location: string;
+    private mobiles: Mobile[]
+    private totalPrice: number;
+
+    constructor(name: string, location: string, mobile: Mobile[], totalPrice: number) {
+        this.name = name;
+        this.location = location;
+        this.mobiles = mobile;
+        this.totalPrice = totalPrice;
+    }
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string) {
+        this.name = name;
+    }
+
+    public getLocation(): string {
+        return this.location;
+    }
+
+    public setLocation(location: string) {
+        this.location = location;
+    }
+
+    public getMobiles(): Mobile[] {
+        return this.mobiles;
+    }
+
+    public setMobiles(mobiles: Mobile[]) {
+        this.mobiles = mobiles;
+    }
+
+    public getTotalPrice(): number {
+        return this.totalPrice;
+    }
+
+    public setTotalPrice(totalPrice: number) {
+        this.totalPrice = totalPrice;
+    }
+    public totalPriceCalculation():number{
+        let price: number = 0
+        for(let i = 0; i< this.mobiles.length; i++){
+            price = this.mobiles[i].getPrice() + price
+            this.totalPrice = price
+        }
+        
+        return this.totalPrice
+    }
+}
