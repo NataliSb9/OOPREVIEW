125d86610fe70664e353101ce3d1a25cf4d4c67c
NataliSb9 <nsainzbarroso@gmail.com>
2021-02-16 21:11:33 +0100

    MobileTest no revisada profe

---
+++ mobileTest.ts
@@ -0,0 +1,15 @@
+import {Mobile} from "./mobile"
+
+let Nokia3210:Mobile = new Mobile("Nokia 3210",3210,"Nokia",6, "azul",false,0,65)
+let iPhone3G: Mobile = new Mobile ("Iphone3G",3,"Apple",8, "negro", false, 2, 120)
+let SamsungGalaxy10: Mobile = new Mobile ("Samsung Galaxy 10", 10, "Samsung", 32,"plata",true, 2, 200 )
+let mobiles: Mobile[] = [Nokia3210,iPhone3G,SamsungGalaxy10];
+
+console.log(Nokia3210.cameraNumber)
+console.log(Nokia3210.is5G)
+console.log(Nokia3210)
+console.log(iPhone3G)
+console.log(SamsungGalaxy10)
+console.log(mobiles)
+Nokia3210.cameraNumber = 4;
+Nokia3210.is5G =true
