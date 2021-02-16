187de8593c00ac0bd34ca8469bae56e6f4a5434f
NataliSb9 <nsainzbarroso@gmail.com>
2021-02-16 23:27:11 +0100

    Mobile.ts cambio en el metodo toString, he quitado el console y he puesto return

--- mobile.ts
+++ mobile.ts
@@ -19,10 +19,6 @@ export class Mobile {
         this.price = price;
     }
 
-
-    public toString() {
-        console.log("The characteristics of the mobile " + this.name + " are:" + "\n" + "  " + "Name: " + this.name + "\n" + "  " + "Model:" + this.model + "\n" + "  " + "Trademark:" + this.trademark + "\n" + "  " + "SD Size (GB):" + this.sdSize + "\n" + "  " + "Color:" + this.color + "\n" + "  " + "Is 5G?:" + this.is5G + "\n" + "  " + "Number of Cameras:" + this.cameraNumber)
-    }
     public getName(): string {
         return this.name;
     }
@@ -86,5 +82,8 @@ export class Mobile {
 
     public setPrice(price: number) {
         this.price = price;
+    }
+    public toString() {
+        return "The characteristics of the mobile " + this.name + " are:" + "\n" + "  " + "Name: " + this.name + "\n" + "  " + "Model:" + this.model + "\n" + "  " + "Trademark:" + this.trademark + "\n" + "  " + "SD Size (GB):" + this.sdSize + "\n" + "  " + "Color:" + this.color + "\n" + "  " + "Is 5G?:" + this.is5G + "\n" + "  " + "Number of Cameras:" + this.cameraNumber
     }
 }
